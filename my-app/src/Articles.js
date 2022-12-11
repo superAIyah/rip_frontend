4689fefa90ea2727e4b821ada272a663d0b4853f
Fedor Safonov <safonovfedya696@gmail.com>
2022-12-11 16:11:29 +0300

    api added

--- my-app/src/Articles.js
+++ my-app/src/Articles.js
@@ -1,12 +1,25 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import { Link } from 'react-router-dom';
 import { useParams} from "react-router-dom";
-import responseData from "./components/data";
+import axios from "axios";
 
 export default function Articles() {
     const {id} = useParams()
 
-    const data = responseData()
+    const [data, setData] = useState([])
+    async function fetchSubjects() {
+        const response = await axios.get('http://127.0.0.1:8000/api/articles/')
+        console.log(response.data)
+        setData(response.data)
+    }
+
+    useEffect( () => {
+            console.log('effect')
+            fetchSubjects()
+        }, []
+    )
+
+    console.log(data)
 
     return (
         <div>
@@ -21,7 +34,7 @@ export default function Articles() {
                     return (
                         <div key={article.title}>
                             <p>{article.title}</p>
-                            <img src={article.photo} height="380" width="500"/>
+                            <img src={article.photo_link} height="380" width="500"/>
                             <br/>
                             <a href="url">{article.link}</a>
                         </div>
--- my-app/src/Subjects.js
+++ my-app/src/Subjects.js
@@ -1,13 +1,28 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import {Link} from "react-router-dom";
+import axios from "axios";
 
 export default function Subjects(){
+
+    const [data, setData] = useState([])
+    async function fetchSubjects() {
+        const response = await axios.get('http://127.0.0.1:8000/api/subjects/')
+        console.log(response.data)
+        setData(response.data)
+    }
+
+    useEffect( () => {
+            console.log('effect')
+            fetchSubjects()
+        }, []
+    )
+
     return(
         <div>
             <ul>
-                <li><Link to="/subjects/1">Computer science</Link></li>
-                <li><Link to="/subjects/2">Physics</Link></li>
-                <li><Link to="/subjects/3">Math</Link></li>
+                <li><Link to="/subjects/1">{data[0]?.name}</Link></li>
+                <li><Link to="/subjects/2">{data[1]?.name}</Link></li>
+                <li><Link to="/subjects/3">{data[2]?.name}</Link></li>
             </ul>
         </div>
     )
