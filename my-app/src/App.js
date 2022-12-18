f7649956f73267b95e4106d463d37fa0bacf17a3
Fedor Safonov <safonovfedya696@gmail.com>
2022-12-19 01:45:36 +0300

    beauty

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,24 @@
 import React from "react"
 import './App.css';
 import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
-import Home from "./Home";
-import Subjects from "./Subjects";
-import Articles from "./Articles";
+import Home from "./components/Home";
+import Subjects from "./components/Subjects";
+import Articles from "./components/Articles";
+import ListGroup from "react-bootstrap/ListGroup";
 
 function App() {
 
     return (
         <BrowserRouter basename="/" >
           <div>
-            <ul>
-              <li>
-                <Link to="/">Старт</Link>
-              </li>
-                <li>
-                    <Link to="/subjects">Предмет</Link>
-                </li>
-            </ul>
+              <ListGroup horizontal>
+                  <ListGroup.Item action href="/">
+                      Start
+                  </ListGroup.Item>
+                  <ListGroup.Item action href="/subjects">
+                      Subjects
+                  </ListGroup.Item>
+              </ListGroup>
             <hr />
             <Routes>
               <Route exact path="/" element={<Home />}>
